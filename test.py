a5ae7327086254a8a02d4077427a13ffb0ce7c7e
hao <xjh5@qq.com>
2013-11-02 21:27:09 +0800

    test

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ class TestFor(unittest.TestCase):
     def test1(self):
         self.assertEqual(1,1)
 
+class Test2(unittest.TestCase):
+    def test3(self):
+        self.assertEqual(3,3)
+                
 if __name__=="__main__":
     unittest.main()
